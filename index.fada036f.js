a1033c72cc376c112a9479fe252c7a70889c3c5a
TDubasova <117123396+TDubasova@users.noreply.github.com>
2023-03-05 18:46:58 +0000

    Deploying to gh-pages from @ TDubasova/project-HTML-CSS@1516461b798b7a84d67894fcc8e91f0c8a7ae6ec 🚀

---
+++ index.fada036f.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtnMobileMenu:document.querySelector("[data-modal-buyNow-mobileMenu-open]"),openModalBtn:document.querySelector("[data-modal-buyNow-open]"),closeModalBtn:document.querySelector("[data-modal-buyNow-close]"),modal:document.querySelector("[data-modal-buyNow]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtnMobileMenu.addEventListener("click",o),e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.fada036f.js.map
