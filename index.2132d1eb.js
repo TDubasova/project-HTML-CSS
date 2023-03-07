3e967d5aa9b33008418ae59cd9ab1bff910f5503
TDubasova <117123396+TDubasova@users.noreply.github.com>
2023-03-07 13:54:28 +0000

    Deploying to gh-pages from @ TDubasova/project-HTML-CSS@f99ec7b379544ae7dd390e5a10a627a72781f2c4 🚀

---
+++ index.2132d1eb.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-readMore-open]"),closeModalBtn:document.querySelector("[data-modal-readMore-close]"),modal:document.querySelector("[data-modal-readMore]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.2132d1eb.js.map
